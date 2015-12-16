e648da53a5957fe9298423c0f26d8f4a3c261b11
nadunindunil <nadun1indunil@gmail.com>
2015-12-16 20:05:05 +0530

    few developments were done

--- index.js
+++ index.js
@@ -72,29 +72,94 @@ app.get('/getCoursesList', function(req, res){
 
 });
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.get('/getTelNumList', function(req, res){
+  //connection.connect();
+
+  connection.query('SELECT * FROM phone_numbers', function(err, rows, fields) {
+    if (err) throw err;
+    res.json(rows);
+    console.log('The solution is: ', rows);
+  });
+  
+  //connection.end();
+  
+
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.get('/getPerfGrpsList', function(req, res){
+  //connection.connect();
+
+  connection.query('SELECT * FROM performance_group', function(err, rows, fields) {
+    if (err) throw err;
+    res.json(rows);
+    console.log('The solution is: ', rows);
+  });
+  
+  //connection.end();
+  
+
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.get('/findTelNum/:id', function(req, res){
+  //connection.connect();
+  var ID = req.params.id;
+  connection.query('SELECT * FROM phone_numbers WHERE number_ID= ?',[ID], function(err, rows, fields) {
+    if (err) throw err;
+    res.json(rows);
+    console.log('The solution is: ', rows);
+  });
+  
+  //connection.end();
+  
+  
+});
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.post('/insertStudent', function (req, res) {
-
+  console.log(req.body.name);
   var post  = {
     ID: req.body.ID, 
     name: req.body.name,
     gender: req.body.gender,
     DOB: req.body.DOB,
     access_level: req.body.access_level,
-    course_ID: req.body.course_ID,
+    course_ID: req.body.courseID,
     performance_group_ID:req.body.performance_group_ID,
     phone_number_id:req.body.phone_number_id };
 
   var query = connection.query('INSERT INTO student SET ?', post, function(err, result) {
     // Neat!
   });
-  console.log(query.sql);
+  //console.log(query.sql);
   res.end('done');
     
 });
 
 //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+app.post('/insertTelNum', function (req, res) {
+
+  var post  = {
+    number_ID: req.body.ID, 
+    location: req.body.location,
+    phone_number: req.body.phone_number
+    };
+
+  var query = connection.query('INSERT INTO phone_numbers SET ?', post, function(err, result) {
+    // Neat!
+  });
+  console.log(query.sql);
+  res.end('done');
+    
+});
 
 // var notifi= new mongoose.Schema({
 //   Time: Date,
